b2b73631170f866b1750b21de774d7b07d156ae0
Joni Lehmuksela <joni.lehmuksela@gmail.com>
2018-12-01 21:36:34 +0200

    First day commits

---
+++ Day 1 Part 1/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open System.IO
+
+let readLines (filePath: string) = seq {
+        use sr = new StreamReader(filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine()
+    }
+
+[<EntryPoint>]
+let main argv =
+    readLines( __SOURCE_DIRECTORY__ + "\\input")
+    |> Seq.map(fun line -> line |> int)
+    |> Seq.sum
+    |> Console.WriteLine
+
+    0
---
+++ Day 1 Part 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_1_Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new List<int>();
+            var sr = new StreamReader(Environment.CurrentDirectory + "\\input");
+            while (!sr.EndOfStream)
+            {
+                input.Add(Convert.ToInt32(sr.ReadLine()));
+            }
+            var i = 0;
+            var rounds = 1;
+            var sum = 0;
+            var prevSums = new List<int>();
+            while(!prevSums.Contains(sum))
+            {
+                prevSums.Add(sum);
+                if (i == input.Count)
+                {
+                    i = 0;
+                    Console.Out.WriteLineAsync($"{rounds.ToString()} rounds done");
+                    rounds++;
+                }
+                sum += input[i];
+                i++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
