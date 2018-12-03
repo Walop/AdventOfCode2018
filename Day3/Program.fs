d91a7d41e0b894832cd1ba4d895937bf93baa1fb
Joni Lehmuksela <10558717+Walop@users.noreply.github.com>
2018-12-03 21:57:57 +0200

    Mega crappy solutions for day 3

---
+++ Day3/Program.fs
@@ -0,0 +1,69 @@
+﻿open System
+open AdventOfCode
+
+let explodeString (s:string) =
+        [for c in s -> c]
+
+
+let implodeString(xs: char list) =
+    let sb = System.Text.StringBuilder xs.Length
+    xs |> List.iter (sb.Append >> ignore)
+    sb.ToString()
+
+let rec getNumbers (nstr: char list, numbers: int list, str: char list) =
+    match str with
+    | [] -> List.append numbers (List.singleton(nstr |> implodeString |> int))
+    | head::tail ->
+        if "1234567890".Contains head then
+            let nnstr = List.append nstr (List.singleton head)
+            getNumbers(nnstr, numbers, tail)
+        else if not (List.isEmpty nstr) then
+            let nnumbers = List.append numbers (List.singleton(nstr |> implodeString |> int))
+            getNumbers(List.empty, nnumbers, tail)
+        else
+            getNumbers(nstr, numbers, tail)
+
+let pointSeq rects = seq {
+    for rect in rects do
+        for x in 0 .. List.item 3 rect - 1 do
+            for y in 0 .. List.item 4 rect - 1 do
+                yield (List.head rect, List.item 1 rect + x, List.item 2 rect + y)
+}
+
+let part1 input =
+    input
+    |> pointSeq
+    |> Seq.groupBy (fun (id, x, y) -> (x,y))
+    |> Seq.filter (fun g -> Seq.length (snd g) > 1)
+    |> Seq.length
+    |> printfn "%d"
+    
+let part2 input =
+    let ids =
+        input
+        |> Seq.map (fun x -> List.head x)
+        |> Set.ofSeq
+
+    input
+    |> pointSeq
+    |> Seq.groupBy (fun (id, x, y) -> (x,y))
+    |> Seq.filter (fun g -> Seq.length (snd g) > 1)
+    |> Seq.map (fun g -> snd g)
+    |> Seq.concat
+    |> Seq.map(fun (id, x, y) -> id)
+    |> Set.ofSeq
+    |> Set.difference ids
+    |> printfn "%A"
+
+[<EntryPoint>]
+let main argv =
+    let input =
+        Util.readLines( __SOURCE_DIRECTORY__ + "\\input")
+        |> Seq.map (fun str -> getNumbers(List.empty, List.empty, str |> explodeString))
+    input
+    |> part1
+
+    input
+    |> part2
+
+    0 // return an integer exit code
