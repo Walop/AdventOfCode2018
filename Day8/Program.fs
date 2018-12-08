7e12acf9b2ffca298af3186dbe3bbceb2252d938
Joni Lehmuksela <10558717+Walop@users.noreply.github.com>
2018-12-08 21:27:36 +0200

    Refactor getMetadatas and solve part 2 of day 8

--- Day8/Program.fs
+++ Day8/Program.fs
@@ -1,16 +1,41 @@
 ﻿open System
 open AdventOfCode
 
-let rec getMetadatas metadatas openNodes input =
+type Node = struct
+    val childcount: int
+    val metadatacount: int
+    val value: int
+    val childvalues: int list
+
+    new (cc, m, v, c) =
+        {childcount = cc; metadatacount = m; value = v; childvalues = c}
+end
+
+let getChildvalue i l =
+    let idx = i - 1
+    List.tryItem idx l |> Option.defaultValue 0
+
+let rec getMetadatas donenodes (openNodes: Node list) mode input =
     if List.isEmpty input then
-        metadatas
+        donenodes
     else
         let lastOpen = List.last openNodes
-        if (fst lastOpen) = 0 then
-            let nmetadatas = List.append metadatas (List.take (snd lastOpen) input)
+        if List.length lastOpen.childvalues = lastOpen.childcount then
+            let nodemetadatas = List.take lastOpen.metadatacount input
+            printfn "%A" nodemetadatas
+            let value =
+                if mode = 1 || lastOpen.childcount = 0 then
+                    List.sum nodemetadatas
+                else
+                    List.sum (List.map (fun m -> getChildvalue m lastOpen.childvalues) nodemetadatas)
+            printfn "Metadata %i, Childcount %i, Value %i, Children: %A" lastOpen.metadatacount lastOpen.childcount value lastOpen.childvalues
+            let ndonenodes = List.append donenodes (List.singleton (Node(lastOpen.metadatacount, lastOpen.childcount, value, lastOpen.childvalues)))
+
             let newLast =
                 if (List.length openNodes) > 1 then
-                    List.item (List.length openNodes - 2) openNodes |> fun n -> (fst n - 1, snd n) |> List.singleton
+                    List.item (List.length openNodes - 2) openNodes
+                    |> fun n -> Node(n.childcount, n.metadatacount, value, List.append n.childvalues (List.singleton value))
+                    |> List.singleton
                 else
                     List.empty
 
@@ -20,22 +45,39 @@ let rec getMetadatas metadatas openNodes input =
                 else
                     openNodes
 
-            getMetadatas nmetadatas nopenNodes (List.skip (snd lastOpen) input)
+            getMetadatas ndonenodes nopenNodes mode (List.skip lastOpen.metadatacount input)
         else
-            let nextNode = List.take 2 input |> fun n -> (n.[0], n.[1])
-            getMetadatas metadatas (List.append openNodes (List.singleton (nextNode))) (List.skip 2 input)
+            let nextNode = List.take 2 input |> fun n -> Node(n.[0], n.[1], 0, List.empty)
+            getMetadatas donenodes (List.append openNodes (List.singleton (nextNode))) mode (List.skip 2 input)
 
 let part1 input =
     let firstNode =
         input
         |> List.take 2
-        |> fun n -> (n.[0], n.[1])
+        |> fun n -> Node(n.[0], n.[1], 0, List.empty)
         |> List.singleton
 
     input
     |> List.skip 2
-    |> (getMetadatas List.empty firstNode)
+    |> (getMetadatas List.empty firstNode 1)
+    |> List.map (fun n -> n.value)
+    //|> printfn "%A"
     |> List.sum
+    |> printfn "%i"
+
+let part2 input =
+    let firstNode =
+        input
+        |> List.take 2
+        |> fun n -> Node(n.[0], n.[1], 0, List.empty)
+        |> List.singleton
+
+    input
+    |> List.skip 2
+    |> (getMetadatas List.empty firstNode 2)
+    |> List.last
+    |> fun n -> n.value
+    //|> printfn "%A"
     |> printfn "%i"
 
 [<EntryPoint>]
@@ -47,7 +89,7 @@ let main argv =
         |> Seq.map int
         |> List.ofSeq
 
-    input |> part1
+    input |> part2
     
 
 
